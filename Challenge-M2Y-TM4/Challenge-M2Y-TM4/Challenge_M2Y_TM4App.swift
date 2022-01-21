2e29e70af3f9cd2285618017a0b480c3cb81e8e7
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-01-21 01:28:07 -0300

    First commit

---
+++ Challenge-M2Y-TM4/Challenge-M2Y-TM4/Challenge_M2Y_TM4App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_M2Y_TM4App.swift
+//  Challenge-M2Y-TM4
+//
+//  Created by Felipe Brigagão de Almeida on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_M2Y_TM4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
